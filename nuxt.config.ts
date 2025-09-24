b2e5fe44a154b6d0436a815c50d511b104cbfcc5
Angelino Verhaeghe <verhaegheangelino@gmail.com>
2025-09-24 20:32:53 +0200

    SC-2 - Add bootstrap 5 and popperjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ["~/assets/scss/main.scss"],
+  css: ["bootstrap/dist/css/bootstrap.css", "~/assets/scss/main.scss"],
   modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/image"],
   vite: {
     css: {
